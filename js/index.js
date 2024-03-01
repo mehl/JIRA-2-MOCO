5b719465911f4cd86b6907ea278a000209605c17
Bastian Frank <git@bastian-frank.de>
2024-03-01 01:02:10 +0100

    error handling

--- js/index.js
+++ js/index.js
@@ -3,13 +3,21 @@ const prompts = require("prompts");
 const loadSheet = require("./sheet.js");
 const render = require("./render.js");
 const sendAll = require("./send.js");
+const ansiColors = require("ansi-colors");
 
 const { argv } = process;
 const file = argv[2];
 
-
 (async () => {
-    const result = loadSheet(file);
+    let result;
+    try {
+        if (!file) throw new Error("Keine Datei angegeben");
+        result = loadSheet(file);
+    } catch (e) {
+        console.log(ansiColors.red.inverse(" Datei konnte nicht geladen werden ") + "\n " + e.message);
+        return;
+    }
+
     render(result);
 
     const response = await prompts([
